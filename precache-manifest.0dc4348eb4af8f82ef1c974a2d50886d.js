25b611863955623b7634e88e141078a852d52783
Arvin Mark Atienza <arvinmarkatienza@arvins-air.rcoffice.ringcentral.com>
2020-01-21 15:24:40 +0800

    Updates

---
+++ precache-manifest.0dc4348eb4af8f82ef1c974a2d50886d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e05aeef5d358ada8684532fa7f3cb57",
+    "url": "/portfolio_01/index.html"
+  },
+  {
+    "revision": "bf993f686d5511ef6684",
+    "url": "/portfolio_01/static/css/main.d15426b2.chunk.css"
+  },
+  {
+    "revision": "14e107fbc9dd3a81dd07",
+    "url": "/portfolio_01/static/js/2.61bf0dc8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio_01/static/js/2.61bf0dc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf993f686d5511ef6684",
+    "url": "/portfolio_01/static/js/main.aa451414.chunk.js"
+  },
+  {
+    "revision": "e7c1b125e63d95911a3f",
+    "url": "/portfolio_01/static/js/runtime-main.e0316071.js"
+  },
+  {
+    "revision": "b829092c5d34db2d6d1a3d2cc142a481",
+    "url": "/portfolio_01/static/media/Vector.b829092c.png"
+  },
+  {
+    "revision": "cd44505836ce3c93dcf4406779c3e949",
+    "url": "/portfolio_01/static/media/undraw-about.cd445058.png"
+  }
+]);
